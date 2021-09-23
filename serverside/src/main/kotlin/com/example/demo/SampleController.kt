e30bbe8e588fb32156f0439f7cb25bc3af6fb76e
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2021-09-24 02:10:52 +0900

    added @CrossOrigin for CORS

--- serverside/src/main/kotlin/com/example/demo/SampleController.kt
+++ serverside/src/main/kotlin/com/example/demo/SampleController.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 class SampleController {
 
     @GetMapping
+    @CrossOrigin
     fun helloWorld(): String {
         return "I got request";
     }
